d0fda82741764eb060b81d65a08ff35c69afc929
Milena <kashaeva.milena@yandex.ru>
2022-12-10 16:46:05 +0500

    Хрень

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿/*Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    Console.WriteLine (a);
+else
+Console.WriteLine (b);*/
+
+/*Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C");
+int c = Convert.ToInt32(Console.ReadLine());
+int ch = 0;
+
+if (ch < a)
+    ch = a;
+if (ch < b)
+    ch = b;
+if (ch < c)
+    ch = c;
+Console.WriteLine (ch);*/
+
+/*Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+Console.WriteLine (a +" Четное");
+else
+Console.WriteLine ("Число нечетное");
+*/
+
+Console.WriteLine("Введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= a)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
+
